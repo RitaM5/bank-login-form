c92a4ea9d396b4ed07ca7fbb098dee5ebbe508cc
RitaM5 <sporshia108514orva@gmail.com>
2022-02-11 03:47:51 +0600

    fifth commit add

--- js/banking.js
+++ js/banking.js
@@ -2,7 +2,7 @@ document.getElementById('deposit-button').addEventListener('click',function(){
     let dipositInput = document.getElementById('deposit-input');
     let newDipositAmountText = dipositInput.value;
     let newDipositAmount = parseFloat(newDipositAmountText);
-
+    //for diposit
     let dipositTotal = document.getElementById('diposit-total');
     let previousDipositAmountText = dipositTotal.innerText ;
     let previousDipositAmount = parseFloat(previousDipositAmountText);
@@ -10,6 +10,40 @@ document.getElementById('deposit-button').addEventListener('click',function(){
     let currentDipositTotalAmount = previousDipositAmount + newDipositAmount ;
 
     dipositTotal.innerText = currentDipositTotalAmount;
-    dipositInput.value = '';
     
+    
+    //for balance
+    let balanceTotal = document.getElementById('balance-total');
+    let previousBalanceAmountText = balanceTotal.innerText ;
+    let previousBalanceAmount = parseFloat(previousBalanceAmountText);
+
+    let currentBalanceTotalAmount = previousBalanceAmount + newDipositAmount ;
+
+    balanceTotal.innerText = currentBalanceTotalAmount;
+    //clear deposit input 
+    dipositInput.value = '';
+});
+
+//for withdraw
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    let withdrawInput = document.getElementById('withdraw-input');
+    let newWithdrawAmountText = withdrawInput.value;
+    let newWithdrawAmount = parseFloat(newWithdrawAmountText);
+    
+    let withdrawTotal = document.getElementById('withdraw-total');
+    let previousWithdrawAmountText = withdrawTotal.innerText ;
+    let previousWithdrawAmount = parseFloat(previousWithdrawAmountText);
+
+    let currentWithdrawTotalAmount = previousWithdrawAmount + newWithdrawAmount ;
+
+    withdrawTotal.innerText = currentWithdrawTotalAmount;
+//update balance
+    let balanceTotal = document.getElementById('balance-total');
+    let previousBalanceText = balanceTotal.innerText ;
+    let previousBalance = parseFloat(previousBalanceText);
+
+    let currentBalanceTotal = previousBalance - currentWithdrawTotalAmount;
+    balanceTotal.innerText = currentBalanceTotal;
+    
+    withdrawInput.value ='';
 })
